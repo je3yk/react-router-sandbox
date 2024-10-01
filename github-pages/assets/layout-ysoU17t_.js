c6f4e0eb7ce6faff59586936e28aeb14a96731f4
Jędrzej Zawojski <32529086+je3yk@users.noreply.github.com>
2024-10-01 13:34:28 +0200

    Delete github-pages directory

--- github-pages/assets/layout-ysoU17t_.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-BIbSRvoA.js";function n(){return t.jsx(t.Fragment,{children:t.jsx(o,{})})}n.displayName="ContactLayoutComponent";export{n as ContactLayoutComponent};
--- github-pages/assets/loader-DpIAoj6u.js
+++
@@ -1,1 +0,0 @@
-import{g as c}from"./index-BIbSRvoA.js";async function n({params:t}){return{contact:await c(t.contactId)}}export{n as contactLoader};
